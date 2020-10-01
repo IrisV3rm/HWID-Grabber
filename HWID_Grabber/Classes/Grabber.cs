9827d630e080afee3913c4ad79aa33b5df02e084
Iris <39096994+IrisV3rm@users.noreply.github.com>
2020-09-30 20:07:02 -0600

    Add files via upload

---
+++ HWID_Grabber/Classes/Grabber.cs
@@ -0,0 +1,185 @@
+﻿
+using System.Management;
+using System.Security.Principal;
+
+namespace Classes
+{
+    public class Grabber
+    {
+        string Result;
+        public string GetOwnerIdentity(bool ReturnDashes = false)
+        {
+            if (ReturnDashes)
+            {
+                return WindowsIdentity.GetCurrent().Owner.Value.ToString();
+            }
+            else
+            {
+                return WindowsIdentity.GetCurrent().Owner.Value.ToString().Replace("-", "");
+            }
+        }
+
+        public string GetCPUID(string ID = "")
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                if (ID == string.Empty)
+                    Result = os["ProcessorId"].ToString();
+                else
+                {
+                    try
+                    {
+                        Result = os[ID].ToString();
+                    }
+                    catch { return "0"; }
+                }
+                break;
+            }
+            searcher.Dispose();
+            return Result;
+        }
+
+        public string GetFanID(string ID = "")
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                if (ID == string.Empty)
+                    Result = os["SerialNumber"].ToString();
+                else
+                {
+                    try
+                    {
+                        Result = os[ID].ToString();
+                    }                    
+                    catch { return "0"; }
+                }
+                break;
+            }
+            searcher.Dispose();
+            return Result;
+        }
+
+        public string GetWin32(string Selection, string ID)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher($"SELECT * FROM {Selection}");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                try
+                {
+                    Result = os[ID].ToString();
+                }
+                catch { return "0"; }
+                break;
+            }
+            searcher.Dispose();
+            return Result;
+        }
+
+        public string GetUSBID(string ID = "")
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_USBHub");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                if (ID == string.Empty)
+                    Result = os["PNPDeviceID"].ToString();
+                else
+                {
+                    try
+                    {
+                        Result = os[ID].ToString();
+                    }
+                    catch { return "0"; }
+                }
+                break;
+            }
+            searcher.Dispose();
+            return Result;
+        }
+
+        public string GetBaseboardID(string ID = "")
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                if (ID == string.Empty)
+                    Result = os["SerialNumber"].ToString();
+                else
+                {
+                    try
+                    {
+                        Result = os[ID].ToString();
+                    }
+                    catch { return "0"; }
+                }
+                break;
+            }
+            searcher.Dispose();
+            return Result;
+        }
+
+        public string GetMemoryID(string ID = "")
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                if (ID == string.Empty)
+                    Result = os["PartNumber"].ToString();
+                else
+                {
+                    try
+                    {
+                        Result = os[ID].ToString();
+                    }
+                    catch { return "0"; }
+                }
+                break;
+            }
+            searcher.Dispose();
+            return Result;
+        }
+
+        public string GetGPUID(string ID = "")
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                if (ID == string.Empty)
+                    Result = os["PNPDeviceID"].ToString();
+                else
+                {
+                    try
+                    {
+                        Result = os[ID].ToString();
+                    }
+                    catch { return "0"; }
+                }
+                break;
+            }
+            searcher.Dispose();
+            return Result;
+        }
+
+        public string GetMonitorID(string ID = "")
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DesktopMonitor");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                if (ID == string.Empty)
+                    Result = os["PNPDeviceID"].ToString();
+                else
+                {
+                    try
+                    {
+                        Result = os[ID].ToString();
+                    }
+                    catch { return "0"; }
+                }
+                break;
+            }
+            searcher.Dispose();
+            return Result;
+        }
+    }
+}
---
+++ HWID_Grabber/Program.cs
@@ -0,0 +1,23 @@
+﻿using Classes;
+using System;
+
+namespace HWID_Grabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grabber grabber = new Grabber();
+            Console.WriteLine(grabber.GetMonitorID());
+            Console.WriteLine(grabber.GetOwnerIdentity());
+            Console.WriteLine(grabber.GetCPUID());
+            Console.WriteLine(grabber.GetFanID());
+            Console.WriteLine(grabber.GetWin32("Win32_IDEController", "PNPDeviceID"));
+            Console.WriteLine(grabber.GetUSBID());
+            Console.WriteLine(grabber.GetBaseboardID());
+            Console.WriteLine(grabber.GetMemoryID());
+            Console.WriteLine(grabber.GetGPUID());
+            Console.ReadKey();
+        }
+    }
+}
